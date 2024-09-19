a64ee69f5ae29a9a5aa4ad3a3b914ddb66ca7ca3
Rabiul Hasan Rafee <rhr277@gmail.com>
2024-09-19 12:07:48 +0600

    backend & front-end  done

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(
+    {
+        origin: ["http://localhost:5173"],
+        credentials: true
+    }
+))
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -9,7 +9,7 @@ router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
 router.post("/verify-email", verifyEmail)
-router.post("/forget-password", forgetPassword)
+router.post("/forgot-password", forgetPassword)
 router.post("/reset-password/:token", resetPassword)
 
 export default router;
